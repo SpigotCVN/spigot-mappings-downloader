78af5a3564793439f3a0f5f24baf7c5d7a755aa6
radsteve <radsteve@radsteve.net>
2024-07-23 13:54:40 +0200

    refactor: switch to gradle kotlin, add publishing

---
+++ build.gradle.kts
@@ -0,0 +1,53 @@
+plugins {
+    id("java")
+    id("com.github.johnrengelman.shadow") version "8.0.0+"
+    id("maven-publish")
+}
+
+group = "io.github.spigotcvn"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+dependencies {
+    // This is required for jgit, we never actually use slf4j
+    implementation("org.slf4j:slf4j-simple:1.7.2")
+    implementation("net.sf.jopt-simple:jopt-simple:5.0.4")
+    implementation("org.eclipse.jgit:org.eclipse.jgit:4.6.0.201612231935-r")
+    implementation("com.google.code.gson:gson:2.11.0")
+    implementation("com.google.guava:guava:33.2.0-jre")
+}
+
+tasks.jar {
+    finalizedBy(tasks.shadowJar)
+    manifest {
+        attributes("Main-Class" to "io.github.spigotcvn.smdownloader.Main")
+    }
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            from(components["java"])
+        }
+    }
+
+    repositories {
+        maven {
+            name = "cvn-repo"
+            url = uri("https://maven.radsteve.net/cvn")
+
+            credentials {
+                username = System.getenv("CVN_MAVEN_USER")
+                password = System.getenv("CVN_MAVEN_TOKEN")
+            }
+        }
+    }
+}
